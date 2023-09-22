70fc9075496fce2d2bc206c13de41576b473f1be
ITESS <ITESS@LOQ-LASB>
2023-09-22 15:31:20 -0600

    gitcommit

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,20 @@
+/*ITESS-TICS
+ *Taller de investigacion
+ *Intento de curso de certificacion java OZI-808
+ *By FJMF
+ *22/Septiembre/2023
+ */
+
+package Main;
+
+/**
+ *
+ * @author ITESS
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
